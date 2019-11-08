88371b74212a6f6c78008be36cbe429e15b4c063
khaled ali <khaled.ali.06.10@gmail.com>
2019-11-08 22:21:05 +0200

    detail_layout

---
+++ app/src/main/java/com/example/egyptguide/FragmentAdapter.java
@@ -0,0 +1,48 @@
+package com.example.egyptguide;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class FragmentAdapter extends FragmentPagerAdapter {
+    private int mFragNum;
+    private Context mContext;
+
+    @Override
+    public int getCount() {
+        return mFragNum;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getStringArray(R.array.Fragments_names)[position];
+    }
+
+    public FragmentAdapter(@NonNull FragmentManager fm, Context context, int fragNum) {
+        super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        mContext = context;
+        mFragNum = fragNum;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0)
+            return new Hotels();
+        else if (position == 1)
+            return new Restaurants();
+        else if (position == 2)
+            return new Entertainment();
+        else if (position == 3)
+            return new Banks();
+        else if (position == 4 )
+            return new Universities();
+        else
+            return  null;
+    }
+}
--- app/src/main/java/com/example/egyptguide/MainActivity.java
+++ app/src/main/java/com/example/egyptguide/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.egyptguide;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager=findViewById(R.id.container);
+        FragmentAdapter fragmentAdapter = new FragmentAdapter(getSupportFragmentManager(), this,5);
+        viewPager.setAdapter(fragmentAdapter);
+        TabLayout tabLayout= findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }

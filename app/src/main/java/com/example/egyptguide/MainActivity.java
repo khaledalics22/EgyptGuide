4ceb6b065c533937596d89991eca655f62b827b5
khaled ali <khaled.ali.06.10@gmail.com>
2019-11-09 16:17:37 +0200

    NavigationDrawer Added

--- app/src/main/java/com/example/egyptguide/MainActivity.java
+++ app/src/main/java/com/example/egyptguide/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.egyptguide;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     public final static int UNIVERSITY = 0;
     public final static int ENTERTAINMENT = 1;
@@ -24,5 +32,55 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(fragmentAdapter);
         TabLayout tabLayout = findViewById(R.id.tab_layout);
         tabLayout.setupWithViewPager(viewPager);
+
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration mAppBarConfiguration;
+        NavigationView navigationView;
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.general, R.id.Famous, R.id.preferred,
+                R.id.feedback, R.id.setting)
+                .setDrawerLayout(drawer)
+                .build();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(getResources().getString(R.string.app_name));
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        MenuItem item = navigationView.getCheckedItem();
+        if (item != null)
+            item.setChecked(false);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        switch (id) {
+            case R.id.general:
+                return true;
+            case R.id.setting:
+                return true;
+            case R.id.feedback:
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{"EgyptGuide.com.test"});
+                email.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
+                email.putExtra(Intent.EXTRA_TEXT, "I have a problem with: ");
+                email.setType("message/rfc822");
+                startActivity(Intent.createChooser(email, "Choose an Email client :"));
+                return true;
+            case R.id.preferred:
+                return true;
+            case R.id.Famous:
+                return true;
+            default:
+                return false;
+        }
     }
 }
